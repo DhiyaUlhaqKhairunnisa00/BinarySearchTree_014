30b0efe1406fb61c4dc1aa9e0b0b63e1aa6aeea2
DhiyaulhaqKhairunnisa00 <dhiyacacaa00@gmail.com>
2022-12-28 17:14:27 +0700

    add public void search

--- BinarySearchTree_014/Program.cs
+++ BinarySearchTree_014/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeNode_014
+{
+	class Node 
+	{
+		public string info;
+		public Node leftchild;
+		public Node rightchild;
+
+		//Constructure for the Node Class
+		public Node(string i, Node l , Node r)
+		{
+			info = i;
+			leftchild = l;
+			rightchild = r;
+		}
+	}
+	/* A Node class consist of the three things, the information,
+	 * references to the right child, and refrences to the left child */
+
+	class Program
+	{
+		public Node ROOT;
+		public Program()
+		{
+			ROOT = null; /* Initializing ROOT to null */
+		}
+		public void search(string element, ref Node parent, ref Node currentNode)
+		{
+
+		}
+	}
+}
